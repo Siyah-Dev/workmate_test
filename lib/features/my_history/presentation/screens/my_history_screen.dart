c921b19a0d68cdaea8b34f3fc42474169df69523
Siyah-Dev <dev.siyah@gmail.com>
2026-01-22 10:17:50 +0530

    bottom nav bar

---
+++ lib/features/my_history/presentation/screens/my_history_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyHistoryScreen extends StatelessWidget {
+  const MyHistoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text("My history"),),);
+  }
+}
